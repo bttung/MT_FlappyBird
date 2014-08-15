2b96ed70767dc087addfa9a0018f4c889039bedc
bttung <tungfcb@gmail.com>
2014-08-15 17:06:29 +0900

    バードの動き、パイプの生成を追加した。

---
+++ FlappyBird/Assets/Scripts/Bird.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+    public Vector2 jumpForce = new Vector2(0, 300);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Jump
+        if (Input.GetKeyUp (KeyCode.Space)) {
+            rigidbody2D.velocity = Vector2.zero;
+            rigidbody2D.AddForce(jumpForce);
+        }
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint (transform.position);
+        if (screenPosition.y > Screen.height || screenPosition.y < 0) {
+            Die();
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+          Die ();
+    }
+
+    void Die() {
+        Debug.Log ("Player died. Reload the game now ...");
+        Application.LoadLevel (Application.loadedLevel);
+    }
+}
---
+++ FlappyBird/Assets/Scripts/PipeMaker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeMaker : MonoBehaviour {
+
+    public GameObject pipes;
+    private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating ("CreatePipe", 1f, 1.5f);
+	}
+	
+    void OnGUI() {
+        GUI.color = Color.black;
+        GUILayout.Label ("Score: " + score.ToString());
+        Debug.Log ("Score: " + score);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreatePipe() {
+        Instantiate (pipes);
+        score++;
+    }
+}
